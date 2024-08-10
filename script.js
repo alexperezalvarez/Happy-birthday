26bd27b03cb730b0fd8a47504511197795a5174c
alexperezalvarez <126110973+alexperezalvarez@users.noreply.github.com>
2024-08-10 12:33:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY', 'DANNA', 'Y', 'QUE', 'SIGAS', 'CUMPLIENDO', 'MUCHOS', 'MAS!'],
+			strings: [ 'HAPPY', 'BIRTHDAY', 'DANNA', '🎂' , '¡Que', 'todos', 'tus', 'deseos', 'se', 'hagan', 'realidad', 'en', 'este', 'día', 'especial!', 'Que', 'la', 'felicidad', 'te', 'acompañe', 'siempre.', '🎈'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
